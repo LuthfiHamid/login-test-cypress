7acedd282806dc188e229662c8ca44b4bd43d0e5
Luthfi Hamid <138122383+LuthfiHamid@users.noreply.github.com>
2024-11-25 21:30:13 +0700

    Update test_login.cy.js

--- newcypress/cypress/e2e/orangeHRM/test login/test_login.cy.js
+++ newcypress/cypress/e2e/orangeHRM/test login/test_login.cy.js
@@ -9,6 +9,21 @@ describe('Login Test in Cypress', () => {
         cy.get('img[alt="client brand banner"]')
           .should('be.visible')
           .log('Login is succesfull')
+    })
+    it.only('Login with invalid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'),
+        cy.get('[name="username"]').type('Admin123');
+        cy.get('[name="password"]').type('admin');
+        cy.get('button[type="submit"]').click();
+        cy.get('[role="alert"]')
+
+    })
+    it.only('Login with invalid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'),
+        cy.get('[name="username"]').type('Admin123');
+        cy.get('[name="password"]');
+        cy.get('button[type="submit"]').click();
+        cy.get('span').contains('Required').should('have.text','Required');
 
     })
 })
